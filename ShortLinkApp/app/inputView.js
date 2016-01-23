85da39324a5d8211004c6d50b963b0af5e19538c
Dmitriy Kornelyuk <dimakorn333@gamil.com>
2016-01-23 11:31:46 +0600

    Main directives are done

--- ShortLinkApp/app/inputView.js
+++ ShortLinkApp/app/inputView.js
@@ -2,8 +2,30 @@
     angular.module("shortLinkApp.inputView", ["ngRoute"])
         .config(["$routeProvider", function ($routeProvider) {
             $routeProvider.when("/inputView", {
-                templateUrl: "templates/inputView.html"
+                template: "<input-view/>"
 
             })
-        }]);
+        }]).directive("inputView", function () {
+            return {
+                templateUrl: "templates/inputView.html",
+                scope: {},
+                bindToController: true,
+                controllerAs:"vm",
+                controller: ["$scope","$http", function ($scope, $http) {
+                    console.log($scope);
+                    this.originalLink = "http://www.google.kz";
+                    this.shortLink = null;
+                    this.onUserInput = (function () {
+                        if (this.shortLink) {
+                            this.shortLink = null;
+                        }
+                    }).bind(this);
+                    
+                    this.onSave = (function () {
+                        console.log("link to save " + this.orginalLink);
+                        this.shortLink = "http://some/2345";
+                    }).bind(this);
+                }]
+            };
+        });;
 })();
--- ShortLinkApp/app/tableView.js
+++ ShortLinkApp/app/tableView.js
@@ -1,9 +1,30 @@
 ﻿(function () {
+    function link(i) {
+        this.orginalLink = "longUrl" + i;
+        this.shortLink= "tinyUrl" + i;
+        this.visitCount = i,
+        this.createDate = "1999-22-11"
+    }
     angular.module("shortLinkApp.tableView", ["ngRoute"])
       .config(["$routeProvider", function ($routeProvider) {
           $routeProvider.when("/tableView", {
-              templateUrl: "templates/tableView.html"
+              template: "<table-view/>"
              
           })
-      }]);
+      }]).directive("tableView", function () {
+          return {
+              templateUrl: "templates/tableView.html",
+              scope: {},
+              bindToController: true,
+              controllerAs: "vm",
+              controller: ["$scope", "$http", function ($scope, $http) {
+                  console.log($scope);
+                  this.links = [];
+                  for (var i = 0; i < 5; ++i) {
+                      this.links.push(new link(i));
+                  }
+              }]
+          }
+
+      });
 })();
